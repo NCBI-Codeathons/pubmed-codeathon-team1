c35400ace65a4b13970e5c20f36995660a04136e
Brian Lee <brian.lee@cdc.gov>
2022-05-24 16:36:46 -0400

    adding fetch notebook and all the pmid raw xmls

--- team1Python/fetch.py
+++ team1Python/fetch.py
@@ -1,10 +1,36 @@
+# I can't get this file to work, so I went over to ../notebooks/fetch_articles_from_pmids.ipynb
+import pandas as pd
+from eutil import EUtils
+from diagnose import print_element
+import math
+from tqdm import tqdm
 
+import sys
+sys.path.append('./teamp1Python')
+
+eutils = EUtils(
+        '8d4c4f67f2a663e9d0ef6ed4d60a4eedd609',  # API key
+        'brian.lee@cdc.gov',  # Email address - unused
+        20,  # API calls per second
+        'https://eutilspreview.ncbi.nlm.nih.gov/entrez'
+        # URL prefix for preview - normally not needed
+    )
+
+def main():
 
 #csv: pmid, query, search_type, page
-    #   search_type: best_match, date
-    #   query:
-    #   pmid:
-    #   page: 1, 2
+#   search_type: best_match, pub_date
+#   query:
+#   pmid:
+#   page: 1, 2
+#df = pd.DataFrame(columns=["pmid", "query", "search_type", "page"])
 
-#add attributes:
-    #   author_name
+    print("iterating through designated queries")
+    df_pmids = pd.read_csv("../data/out/pmids.csv")
+    for pmid in tqdm(df_term["pmid"]):
+        print(pmid)
+
+
+if __name__ == '__main__':
+    main()
+
